cb55eb2b681ba48d8cba222939fcb00065872d64
weihongbo <weihongbo@xkw.cn>
2025-11-24 14:47:13 +0800

    update data

--- app/src/main/java/com/example/leetcode_ex/Ex2.kt
+++ app/src/main/java/com/example/leetcode_ex/Ex2.kt
@@ -1,11 +1,15 @@
 package com.example.leetcode_ex
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.PriorityQueue
+
 /**
  * 链表：
- * 前一个和后一个组成一对键值表，和HashMap配合
+ * 前一个和后一个组成一对键值表，和HashMap配合 和优先队列配合排序
  */
 fun main(args: Array<String>) {
-
 }
 
 /**
@@ -103,6 +107,46 @@ fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
     }
 
     return result.next
+}
+
+/**
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ *
+ *
+ * 解题：
+ * 1、借助优先级队列，取出来数组的头全部存入队列中
+ * 2、因为放进去已经排好顺序，最顶就是最小，取出一个开始建立连接，再将next放进队列中，直到取完
+ *
+ * 1、二分法，两个两个合并，利用递归返回
+ */
+@RequiresApi(Build.VERSION_CODES.N)
+fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    if (lists == null) return null
+    if (lists.size == 1) return lists.get(0)
+
+
+    val head = ListNode(0)
+    var temp = head
+    val p = PriorityQueue<ListNode> { a, b -> a.`val` - b.`val` }
+
+    for (i in 0..lists.size - 1) {
+        if (lists[i] != null) {
+            p.add(lists[i])
+        }
+    }
+
+
+    while (p.size > 0) {
+        val n = p.poll()
+        if (n.next != null) {
+            p.add(n.next)
+        }
+        temp.next = n
+        temp = temp.next!!
+    }
+
+    return head.next
 }
 
 /**
@@ -245,10 +289,9 @@ fun deleteNode(node: ListNode?) {
  */
 fun oddEvenList(head: ListNode?): ListNode? {
     if (head == null || head.next == null || head.next!!.next == null) return head
-
     var aa: ListNode? = head
     var bb: ListNode? = head!!.next
-    var ss :ListNode? = bb
+    var ss: ListNode? = bb
     var cur = head.next!!.next
 
     var flag = true
@@ -256,9 +299,9 @@ fun oddEvenList(head: ListNode?): ListNode? {
         if (flag) {
             aa!!.next = cur
             aa = aa.next
-        }else{
-            bb!!.next =cur
-            bb =bb!!.next
+        } else {
+            bb!!.next = cur
+            bb = bb!!.next
         }
         flag = !flag
 
@@ -270,10 +313,43 @@ fun oddEvenList(head: ListNode?): ListNode? {
 }
 
 /**
+ * 2181 合并之间的节点
+ * 输入：head = [0,3,1,0,4,5,2,0]
+ * 输出：[4,11]
+ */
+fun mergeNodes(head: ListNode?): ListNode? {
+    if (head == null) return null
+
+    val temp = ListNode(0)
+    var result = temp
+
+    var cur = head
+    var sum = 0
+    while (cur != null) {
+        val x = cur.`val`
+        if (x == 0) {
+            if (sum != 0) {
+                result.next = ListNode(sum)
+                result = result.next!!
+                sum = 0
+            }
+        } else {
+            sum += x
+        }
+        cur = cur.next
+    }
+    return temp.next
+}
+
+/**
  * 插入排序，先查找，然后从头开始遍历找到位置 o(n2)
+ * 输入: head = [4,2,1,3]
+ * 输出: [1,2,3,4]
+ *
+ * 解题
  */
 //fun insertionSortList(head: ListNode?): ListNode? {
-//
+//    if (head == null || head.next == null) return head
 //}
 
 /**
@@ -293,7 +369,6 @@ fun oddEvenList(head: ListNode?): ListNode? {
  *
  */
 class LRUCache(val capacity: Int) {
-
     val map = mutableMapOf<Int, Node>()
 
     class Node(val v: Int) {
